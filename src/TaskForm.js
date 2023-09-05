f95b9e8a212071ef9f943ad468df53d4da1afdc7
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-06 11:44:57 +1200

    Add state for form

---
+++ src/TaskForm.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function TaskForm() {
+  const [formData, setFormData] = useState({
+    task: '',
+    units: '',
+    endDate: '',
+    unitsPerDay: '',
+    startDate: '',
+  })
+
+  return <></>
+}
+
+export default TaskForm
