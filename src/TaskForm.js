5747576d4d8d86d1933d63d18e0d3997ffd50665
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-06 14:06:56 +1200

    Add introductory text to first three input elements

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -20,6 +20,10 @@ function TaskForm() {
     <>
       <form>
         <div>
+          <p>
+            What is the name of your task? This might be the name of a project
+            you want to complete, a book you want to read, or something else.
+          </p>
           <input
             type="text"
             name="task"
@@ -29,19 +33,29 @@ function TaskForm() {
           ></input>
         </div>
         <div>
-          <input
-            type="number"
-            name="unitsNum"
-            value={inputData.unitsNum}
-            onChange={handleChange}
-            aria-label="Number of units:"
-          ></input>
+          <p>
+            What kind of daily target do you want to set for completing your
+            task? You might set a number of minutes per day, a number of pages
+            per day, or something else
+          </p>
           <input
             type="text"
             name="unitsName"
             value={inputData.unitsName}
             onChange={handleChange}
             aria-label="Name of units"
+          ></input>
+        </div>
+        <div>
+          <p>
+            How many of these units will it take in total to complete your task?
+          </p>
+          <input
+            type="number"
+            name="unitsNum"
+            value={inputData.unitsNum}
+            onChange={handleChange}
+            aria-label="Number of units:"
           ></input>
         </div>
         <div>
