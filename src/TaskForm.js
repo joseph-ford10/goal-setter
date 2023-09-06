f4b4d5c658c7d620b49d993f12e118e5b1d1f730
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-06 15:13:26 +1200

    Add conditional rendering for input elements

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -31,82 +31,94 @@ function TaskForm() {
   return (
     <>
       <form>
-        <div>
-          <p>
-            What is the name of your task? This might be the name of a project
-            you want to complete, a book you want to read, or something else.
-          </p>
-          <input
-            type="text"
-            name="task"
-            value={inputData.task}
-            onChange={handleChange}
-            aria-label="Task"
-          ></input>
-          <DoneButton formProgressSetter={formProgressSetter} number="2" />
-        </div>
-        <div>
-          <p>
-            What kind of daily target do you want to set for completing your
-            task? You might use a time unit like "minutes" or a different unit
-            like "pages".
-          </p>
-          <input
-            type="text"
-            name="unitsName"
-            value={inputData.unitsName}
-            onChange={handleChange}
-            aria-label="Name of units"
-          ></input>
-          <DoneButton formProgressSetter={formProgressSetter} number="3" />
-        </div>
-        <div>
-          <p>
-            How many {inputData.unitsName} will it take in total to complete
-            your task?
-          </p>
-          <input
-            type="number"
-            name="unitsNum"
-            value={inputData.unitsNum}
-            onChange={handleChange}
-            aria-label="Number of units:"
-          ></input>
-          <DoneButton formProgressSetter={formProgressSetter} number="4" />
-        </div>
-        <div>
-          <p>What is your end date?</p>
-          <input
-            type="date"
-            name="endDate"
-            value={inputData.endDate}
-            onChange={handleChange}
-            aria-label="End date"
-          ></input>
-          <DoneButton formProgressSetter={formProgressSetter} number="5" />
-        </div>
-        <div>
-          <p>How many {inputData.unitsName} per day can you do?</p>
-          <input
-            type="number"
-            name="unitsPerDay"
-            value={inputData.unitsPerDay}
-            onChange={handleChange}
-            aria-label="Units per day"
-          ></input>
-          <DoneButton formProgressSetter={formProgressSetter} number="5" />
-        </div>
-        <div>
-          <p>When do you want to start working towards your goal?</p>
-          <input
-            type="date"
-            name="startDate"
-            value={inputData.startDate}
-            onChange={handleChange}
-            aria-label="Start date"
-          ></input>
-          <DoneButton formProgressSetter={formProgressSetter} number="6" />
-        </div>
+        {formProgress > 0 && (
+          <div>
+            <p>
+              What is the name of your task? This might be the name of a project
+              you want to complete, a book you want to read, or something else.
+            </p>
+            <input
+              type="text"
+              name="task"
+              value={inputData.task}
+              onChange={handleChange}
+              aria-label="Task"
+            ></input>
+            <DoneButton formProgressSetter={formProgressSetter} number={2} />
+          </div>
+        )}
+        {formProgress > 1 && (
+          <div>
+            <p>
+              What kind of daily target do you want to set for completing your
+              task? You might use a time unit like "minutes" or a different unit
+              like "pages".
+            </p>
+            <input
+              type="text"
+              name="unitsName"
+              value={inputData.unitsName}
+              onChange={handleChange}
+              aria-label="Name of units"
+            ></input>
+            <DoneButton formProgressSetter={formProgressSetter} number={3} />
+          </div>
+        )}
+        {formProgress > 2 && (
+          <div>
+            <p>
+              How many {inputData.unitsName} will it take in total to complete
+              your task?
+            </p>
+            <input
+              type="number"
+              name="unitsNum"
+              value={inputData.unitsNum}
+              onChange={handleChange}
+              aria-label="Number of units:"
+            ></input>
+            <DoneButton formProgressSetter={formProgressSetter} number={4} />
+          </div>
+        )}
+        {formProgress > 3 && (
+          <div>
+            <p>What is your end date?</p>
+            <input
+              type="date"
+              name="endDate"
+              value={inputData.endDate}
+              onChange={handleChange}
+              aria-label="End date"
+            ></input>
+            <DoneButton formProgressSetter={formProgressSetter} number={5} />
+          </div>
+        )}
+        {formProgress > 3 && (
+          <div>
+            <p>How many {inputData.unitsName} per day can you do?</p>
+            <input
+              type="number"
+              name="unitsPerDay"
+              value={inputData.unitsPerDay}
+              onChange={handleChange}
+              aria-label="Units per day"
+            ></input>
+            <DoneButton formProgressSetter={formProgressSetter} number={5} />
+          </div>
+        )}
+        {formProgress > 4 && (
+          <div>
+            <p>When do you want to start working towards your goal?</p>
+            <input
+              type="date"
+              name="startDate"
+              value={inputData.startDate}
+              onChange={handleChange}
+              aria-label="Start date"
+            ></input>
+            <DoneButton formProgressSetter={formProgressSetter} number={6} />
+          </div>
+        )}
       </form>
     </>
   )
