cbc2202b14b9879ce12e8f511d386274d3cfc082
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-06 12:00:15 +1200

    Add form for collecting goal data

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -26,6 +26,41 @@ function TaskForm() {
           value={inputData.task}
           onChange={handleChange}
         ></input>
+        <label>Unit number:</label>
+        <input
+          type="number"
+          name="unitsNum"
+          value={inputData.unitsNum}
+          onChange={handleChange}
+        ></input>
+        <label>Units name:</label>
+        <input
+          type="text"
+          name="unitsName"
+          value={inputData.unitsName}
+          onChange={handleChange}
+        ></input>
+        <label>End date:</label>
+        <input
+          type="date"
+          name="endDate"
+          value={inputData.endDate}
+          onChange={handleChange}
+        ></input>
+        <label>Units per day:</label>
+        <input
+          type="number"
+          name="unitsPerDay"
+          value={inputData.unitsPerDay}
+          onChange={handleChange}
+        ></input>
+        <label>Start date:</label>
+        <input
+          type="date"
+          name="startDate"
+          value={inputData.startDate}
+          onChange={handleChange}
+        ></input>
       </form>
     </>
   )
