e7b91f48d34bd951349fc6616624bba155db6827
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-06 14:01:14 +1200

    Group form inputs into divs

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -19,48 +19,58 @@ function TaskForm() {
   return (
     <>
       <form>
-        <input
-          type="text"
-          name="task"
-          value={inputData.task}
-          onChange={handleChange}
-          aria-label="Task"
-        ></input>
-        <input
-          type="number"
-          name="unitsNum"
-          value={inputData.unitsNum}
-          onChange={handleChange}
-          aria-label="Number of units:"
-        ></input>
-        <input
-          type="text"
-          name="unitsName"
-          value={inputData.unitsName}
-          onChange={handleChange}
-          aria-label="Name of units"
-        ></input>
-        <input
-          type="date"
-          name="endDate"
-          value={inputData.endDate}
-          onChange={handleChange}
-          aria-label="End date"
-        ></input>
-        <input
-          type="number"
-          name="unitsPerDay"
-          value={inputData.unitsPerDay}
-          onChange={handleChange}
-          aria-label="Units per day"
-        ></input>
-        <input
-          type="date"
-          name="startDate"
-          value={inputData.startDate}
-          onChange={handleChange}
-          aria-label="Start date"
-        ></input>
+        <div>
+          <input
+            type="text"
+            name="task"
+            value={inputData.task}
+            onChange={handleChange}
+            aria-label="Task"
+          ></input>
+        </div>
+        <div>
+          <input
+            type="number"
+            name="unitsNum"
+            value={inputData.unitsNum}
+            onChange={handleChange}
+            aria-label="Number of units:"
+          ></input>
+          <input
+            type="text"
+            name="unitsName"
+            value={inputData.unitsName}
+            onChange={handleChange}
+            aria-label="Name of units"
+          ></input>
+        </div>
+        <div>
+          <input
+            type="date"
+            name="endDate"
+            value={inputData.endDate}
+            onChange={handleChange}
+            aria-label="End date"
+          ></input>
+        </div>
+        <div>
+          <input
+            type="number"
+            name="unitsPerDay"
+            value={inputData.unitsPerDay}
+            onChange={handleChange}
+            aria-label="Units per day"
+          ></input>
+        </div>
+        <div>
+          <input
+            type="date"
+            name="startDate"
+            value={inputData.startDate}
+            onChange={handleChange}
+            aria-label="Start date"
+          ></input>
+        </div>
       </form>
     </>
   )
