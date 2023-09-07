9d9b43ae151e149244ecd812af778bb1ec3c5ef9
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-07 14:14:53 +1200

    Add explanatory text to homescreen

--- src/Title.js
+++ src/Title.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 function Title() {
-  return <h1>Goal Setter</h1>
+  return (
+    <>
+      <h1>Goal Setter</h1>
+      <p>
+        Welcome to Goal Setter. Goal Setter is a motivational tool for
+        completing projects or tasks. You can either:
+      </p>
+      <ol>
+        <li>
+          Set a daily target and have Goal Setter tell you how long it will take
+          to complete the task if you stick to that target.
+        </li>
+        <li>
+          Set a completion date and have Goal Setter tell you what your daily
+          target should be if you want to be done in time.
+        </li>
+      </ol>
+
+      <p>
+        Sound good? All you have to do is enter some basic information about
+        your project task. Click below to get started.
+      </p>
+    </>
+  )
 }
 
 export default Title
