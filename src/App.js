a15d2bcb175657c338ea4258feacd7165c5a0055
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-07 14:05:51 +1200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Title from './Title'
 import TaskForm from './TaskForm'
 
 function App() {
   return (
     <div className="App">
-      <Title />
-      <TaskForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Title />}></Route>
+          <Route path="/form" element={<TaskForm />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
