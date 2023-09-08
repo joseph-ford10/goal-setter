4527b98c931e0d022f6c0a3be5779b7257e3f827
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-09 11:00:31 +1200

    Style homepage

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [introText, setIntroText] = useState(1)
+
   return (
     <>
-      <p>
-        Welcome to Goal Setter. Goal Setter is a motivational tool for
-        completing projects or tasks. You can either:
-      </p>
-      <ol>
-        <li>
-          Set a daily target and have Goal Setter tell you how long it will take
-          to complete the task if you stick to that target.
-        </li>
-        <li>
-          Set a completion date and have Goal Setter tell you what your daily
-          target should be if you want to be done in time.
-        </li>
-      </ol>
-
-      <p>
-        Sound good? All you have to do is enter some basic information about
-        your project task. Click below to get started.
-      </p>
+      <section>
+        {introText === 1 && (
+          <div className="introText">
+            <p>Welcome to Goal Setter.</p>
+            <button className="nextButton" onClick={() => setIntroText(2)}>
+              Next
+            </button>
+          </div>
+        )}
+        {introText === 2 && (
+          <div className="introText">
+            <p>
+              Goal Setter is a motivational tool for completing projects or
+              tasks. It works in two ways.
+            </p>
+            <button className="nextButton" onClick={() => setIntroText(3)}>
+              Next
+            </button>
+          </div>
+        )}
+        {introText === 3 && (
+          <div className="introText">
+            <p>
+              You can set a daily target and have Goal Setter tell you how long
+              it will take you to complete your task if you stick to that
+              target.
+            </p>
+            <button className="nextButton" onClick={() => setIntroText(4)}>
+              Next
+            </button>
+          </div>
+        )}
+        {introText === 4 && (
+          <div className="introText">
+            <p>
+              Or you can set a completion date and have Goal Setter tell you
+              what your daily target should be if you want to be done in time.
+            </p>
+            <button className="nextButton" onClick={() => setIntroText(5)}>
+              Next
+            </button>
+          </div>
+        )}
+        {introText === 5 && (
+          <div className="introText">
+            <p>
+              Sound good? All you have to do is enter some basic information
+              about your project or task. Click below to get started.
+            </p>
+            <button className="nextButton" onClick={() => setIntroText(6)}>
+              Next
+            </button>
+          </div>
+        )}
+      </section>
     </>
   )
 }
