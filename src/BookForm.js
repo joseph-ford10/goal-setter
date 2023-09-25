93bc1ac351ce7bf0efc1762c44810700f7aecda6
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-25 18:17:05 +1300

    Add footer to form

--- src/BookForm.js
+++ src/BookForm.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const BookForm = () => {
-  return <div>BookForm</div>
+  return (
+    <>
+      <div>BookForm</div>
+      <footer>
+        <p>
+          Icon made by{' '}
+          <a href="https://www.flaticon.com/authors/freepik">Freepik</a> from{' '}
+          <a href="https://www.flaticon.com">flaticon.com</a>
+        </p>
+        <p>
+          Icon made by{' '}
+          <a href="https://www.flaticon.com/authors/handicon">Handicon</a> from{' '}
+          <a href="https://www.flaticon.com">flaticon.com</a>
+        </p>
+      </footer>
+    </>
+  )
 }
 
 export default BookForm
