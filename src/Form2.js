a631e374b5b94bbf2c9dd0954253805f1ca4a026
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-26 11:10:17 +1300

    Add form page 2

--- src/Form2.js
+++ src/Form2.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Form2 = ({ text, handleChange, inputData }) => {
-  return <div>{text}</div>
+  const navigate = useNavigate()
+  return (
+    <>
+      <p className="formText">{text}</p>
+      <div className="inputContainer">
+        <input
+          type="number"
+          name="pages"
+          value={inputData.pages}
+          onChange={handleChange}
+          aria-label="Number of pages:"
+          placeholder="enter a number"
+        ></input>
+      </div>
+      <section className="buttonContainer">
+        <button onClick={() => navigate('/form/page3')}>Hello World</button>
+      </section>
+    </>
+  )
 }
 
 export default Form2
