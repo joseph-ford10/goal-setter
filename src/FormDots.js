9516fde425b61048f9803f3b11180d04b5d49bfb
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-09-27 09:19:45 +1300

    Remove unneeded files

--- src/FormDots.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const FormDots = () => {
-  return <div>FormDots</div>
-}
-
-export default FormDots
--- src/Results1.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Results1 = () => {
-  return <div>Hello world!</div>
-}
-
-export default Results1
